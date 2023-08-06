68d1cfed501ba2cc1c900e24ac6cf866842753b9
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-08-06 19:57:10 +0530

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const About = () => {
 
@@ -70,4 +71,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About,'about')
